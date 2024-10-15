0b52bf04344184ea35ab623752fcc186d5bb18be
senhorvinicius <vestanzani@gmail.com>
2024-10-15 10:17:59 -0300

    Update and rename head.js to header

--- head.js
+++
@@ -1,7 +0,0 @@
-document.write(`
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Título da Página</title>
-    <link rel="stylesheet" href="style.css">
-    <link rel="icon" href="favicon.ico">
-`);
