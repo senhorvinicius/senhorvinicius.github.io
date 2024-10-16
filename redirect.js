9e5ce973c1701c35d6f5951ac9a89ddc112481ef
senhorvinicius <vestanzani@gmail.com>
2024-10-16 00:37:44 -0300

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,6 +1,6 @@
 // redirect.js
  (function() {
-    var redirectUrl = "https:// www.novosite.com"; // URL para redirecionar
+//    var redirectUrl = "https:// www.novosite.com"; // URL para redirecionar
 
     // Condição de redirecionamento (mude 'true' para sua condição)
     if (true) { // Exemplo: true ou alguma outra condição
