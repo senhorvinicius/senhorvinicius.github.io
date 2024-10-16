5ee8e011b276ed3bd7db4c0c1037a1f7d6c71201
senhorvinicius <vestanzani@gmail.com>
2024-10-16 01:44:27 -0300

    Update header.js

--- header.js
+++ header.js
@@ -16,5 +16,20 @@
     // Adiciona o cabeçalho ao contêiner
     document.addEventListener("DOMContentLoaded", function() {
         document.getElementById('headerContainer').innerHTML = headerHTML;
+
+        // Adiciona a tag do Google Analytics ao cabeçalho
+        var gtagScript = document.createElement('script');
+        gtagScript.async = true;
+        gtagScript.src = "https://www.googletagmanager.com/gtag/js?id=G-68NNQZD8FS";
+        document.head.appendChild(gtagScript);
+        
+        var gtagInit = document.createElement('script');
+        gtagInit.innerHTML = `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-68NNQZD8FS');
+        `;
+        document.head.appendChild(gtagInit);
     });
 })();
