0793fd6059ff29eed05324cf93d3f355e75e74e8
senhorvinicius <vestanzani@gmail.com>
2024-10-16 00:23:02 -0300

    Create redirect.js

---
+++ redirect.js
@@ -0,0 +1,9 @@
+// redirect.js
+(function() {
+    var redirectUrl = "https://www.novosite.com"; // URL para redirecionar
+
+    // Condição de redirecionamento (mude 'true' para sua condição)
+    if (true) { // Exemplo: true ou alguma outra condição
+        window.location.href = redirectUrl;
+    }
+})();
